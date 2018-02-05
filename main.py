e01f374eda73e3ebd876f44dcb16cc8002d5acb3
Peter554 <byfield554@gmail.com>
2018-02-05 16:56:07 +0000

    loss attempt

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from torchvision import datasets, transforms
 from torch.autograd import Variable
 
 from modules import my_vgg, ae_module, loss_module
+import os
 
 ###
 
@@ -15,6 +16,8 @@ content_layers = ['r11', 'r21', 'r31']
 
 cuda = torch.cuda.is_available()
 dtype = torch.cuda.FloatTensor if cuda else torch.FloatTensor
+
+print_every = 10
 
 ###
 
@@ -31,7 +34,7 @@ train_loader = torch.utils.data.DataLoader(dataset, batch_size=batch_size, shuff
 
 ###
 
-auto_encoder = ae_module.Auto_Encoder()
+auto_encoder = ae_module.Auto_Encoder(input_channels=3, bn_momentum=0.9)
 vgg = my_vgg.VGG()
 content_loss = loss_module.Content_Loss()
 
@@ -49,17 +52,23 @@ adam = torch.optim.Adam(auto_encoder.parameters(), lr=learning_rate)
 
 ###
 
-for e in epochs:
+for e in range(epochs):
+    print('Epoch {} of {}'.format(e + 1, epochs))
 
     auto_encoder.train()
+    loss_counter = 0.
     for i, (images, _) in enumerate(train_loader):
+        if i % print_every == 0: print('Batch {} of {}'.format(i + 1, len(train_loader)))
         images = Variable(images.type(dtype))
         adam.zero_grad()
         target = vgg(images, out_keys=content_layers)
         reconstruced, mean, logvar = auto_encoder(images, out_keys=['reconstructed', 'mean', 'logvar'])
         output = vgg(reconstruced, out_keys=content_layers)
         loss = content_loss(output, target, mean, logvar)
+        loss_counter += loss.data
         loss.backward()
         adam.step()
+    print('Average loss over epoch = {}'.format(loss_counter / (i + 1)))
 
-        c = 2
+    os.makedirs('./models', exist_ok=True)
+    torch.save(auto_encoder.state_dict(), './ae_params_epoch{}.pt'.format(e + 1))
